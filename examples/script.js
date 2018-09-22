5220f81a6978ca1f7a1c8e3a7a27ccd479999a8a
Amir Off <ameer157@gmail.com>
2018-09-22 13:14:19 +0300

    ES6 optimization

--- examples/script.js
+++ examples/script.js
@@ -1,59 +1,70 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 
-  // Create new aREST device when button is clicked
-  $('#validate').click(function() {
-    var address = $('#device_address').val();
-    var device = new Device(address);
+    // Create new aREST device when button is clicked
+    $('#validate').click(() => {
+        let address = $('#device_address').val();
+        let device = new Device(address);
 
-    // Set device pins
-    device.pinMode(6, "o");
-    device.pinMode(2, "i");
+        // Set device pins
+        device.pinMode(6, "o");
+        device.pinMode(2, "i");
 
-    // Button
-    $('#on').click(function() {
-      device.digitalWrite(6, 1);
+        // Call func
+        $('#call').click(async () => {
+            let val = $('#functionName').val();
+            await device.callFunction(val, 1);
+        });
+
+        // Button
+        $('#on').click(async () => {
+            await device.digitalWrite(5, 1);
+            device.digitalRead(5, (data) => {
+                $('#5').html(data.return_value);
+            });
+        });
+
+        $('#off').click(async () => {
+            await device.digitalWrite(5, 0);
+            device.digitalRead(5, (data) => {
+                $('#5').html(data.return_value);
+            });
+        });
+
+        // Analog write
+        $('#slider').mouseup(() => {
+            let val = $('#slider').val();
+            device.analogWrite(6, val);
+        });
+
+        //Analog read every 5 seconds
+        device.analogRead(0, (data) => {
+            $("#A0").html(data.return_value);
+        });
+        setInterval(() => {
+            device.analogRead(0, (data) => {
+                $("#A0").html(data.return_value);
+            });
+        }, 5000);
+
+        // Digital read every 5 seconds
+        device.digitalRead(5, (data) => {
+            $('#5').html(data.return_value);
+        });
+        setInterval(() => {
+            device.digitalRead(5, (data) => {
+                $('#5').html(data.return_value);
+            });
+        }, 5000);
+
+        // Temperature display
+        device.getVariable('temperature', (data) => {
+            $('#temperature').html(data.temperature);
+        });
+
+        // Humidity display
+        device.getVariable('humidity', (data) => {
+            $('#humidity').html(data.humidity);
+        });
     });
-
-    $('#off').click(function() {
-      device.digitalWrite(6, 0);
-    });
-
-    // Analog write
-    $('#slider').mouseup(function() {
-      var val = $('#slider').val();
-      device.analogWrite(6, val);
-    });
-
-    //Analog read every 5 seconds
-    device.analogRead(0, function(data) {
-      $("#A0").html(data.return_value);
-    });
-    setInterval(function() {
-      device.analogRead(0, function(data) {
-        $("#A0").html(data.return_value);
-      });
-    }, 5000);
-
-    // Digital read every 5 seconds
-    device.digitalRead(11, function(data) {
-      $('#2').html(data.return_value);
-    });  
-    setInterval(function() {
-      device.digitalRead(11, function(data) {
-        $('#2').html(data.return_value);
-      });
-    }, 5000);
-
-    // Temperature display
-    device.getVariable('temperature', function(data) {
-      $('#temperature').html(data.temperature);
-    });
-
-    // Humidity display
-    device.getVariable('humidity', function(data) {
-      $('#humidity').html(data.humidity);
-    });
-  });
 
 });
-
